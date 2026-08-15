b5c668d96a29610791b54a5829ff3238d8aae553
Dharani <vijayakumardharani78@gmail.com>
2026-08-15 20:55:11 +0530

    Connect database to cloud

--- db.js
+++ db.js
@@ -1,19 +1,30 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Dharani@143",
-    database: "resumenova_ai"
+const connection = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+
+    ssl: {
+        rejectUnauthorized: false
+    },
+
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-connection.connect((err) => {
+connection.getConnection((err, conn) => {
     if (err) {
         console.log("Database connection failed ❌");
+        console.log(err);
         return;
     }
 
-    console.log("MySQL Database Connected Successfully ✅");
+    console.log("MariaDB Database Connected Successfully ✅");
+    conn.release();
 });
 
 module.exports = connection;
